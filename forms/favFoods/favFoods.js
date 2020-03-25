4b2dcd27583a21202a42c294595032dc7fbe1ab5
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-03-25 10:46:38 -0500

    new code

--- forms/favFoods/favFoods.js
+++ forms/favFoods/favFoods.js
@@ -1,1 +1,23 @@
 let goodFoods = ["pizza","apples","cookies","sushi","pasta","steak"]
+
+// populate the list using foods array
+favFoods.onshow=function(){
+    // always clear before populating
+    lstFoods.clear()   
+    // put array of foods in the dropdown
+    for (i = 0; i <= goodFoods.length - 1; i++) 
+        lstFoods.addItem(goodFoods[i])
+}
+
+lstFoods.onclick=function(goodFoods){
+  if (typeof(goodFoods) == "object") {   // user clicked the control
+    return
+  } else {
+  let textChoice = NSB.$("lstFoods_" + goodFoods).textContent
+  lbl.Message.value = `You picked ${textChoice} - that is a great choice!`
+ }
+}
+
+btnDessert.onclick=function () {
+ChangeForm(dessertVoting)
+}
--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,8 +1,23 @@
-
 hmbMenu.onclick=function(s){
-  if (typeof(s) == "object") 
+  if (typeof(s) == "object") {
     return
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
+    } else {
+    switch(s) {
+        case 'Login':
+            ChangeForm(loginCU)
+        break
+        case 'Favorite Foods':
+            ChangeForm(favFoods)
+        break
+        case 'Dessert Voting':
+            ChangeForm(dessertVoting) 
+        break
+        case 'Describe Me':
+            ChangeForm(describeYou)
+        break
+        case 'AB Favorite Exercises':
+            ChangeForm(ABfavExercises)
+        break
+        }
+    }
 }
