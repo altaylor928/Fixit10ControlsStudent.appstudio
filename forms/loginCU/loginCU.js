ead539390aa662a294bb6df2edd04c84268f1244
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-03-25 19:25:01 -0500

    updates

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,13 +1,12 @@
-let req1 = []
+let req1 = [ ]
 
 btnLogin.onclick=function(){
   req1 = Ajax("http://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptPassword.value)
-if (req1.status == 200) { //everthing worked
+if (req1.status == 200) {
      lblResponse.hidden = false
-     lblResponse.value = (`Your LDAP return code was ` + req1.responseText) //1 good, 0 bad
+     lblResponse.value = (`Your LDAP return code was ` + req1.responseText)
      NSB.MsgBox(lblResponse.value)
 } else {
-     // had problem with AJAX request
      lblResponse.hidden = false
      lblResponse.value = (`Error: ` + req1.status)
      NSB.MsgBox(lblResponse.value)
