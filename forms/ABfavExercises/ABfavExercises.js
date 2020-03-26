2df1ff7db217b633029f275a38062891d63c2f14
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-03-26 18:23:36 -0500

    updates

--- forms/ABfavExercises/ABfavExercises.js
+++ forms/ABfavExercises/ABfavExercises.js
@@ -1,9 +1,13 @@
 var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
+// populate the list using exercises array
 ABfavExercises.onshow=function(){
+   // always clear before populating
   selExercises.clear()
+  // put array of desserts in the dropdown
   for(i=0; i<= exercises.length -1; i++)
+  selExercises.addItem(exercises[i]) 
 }
 
 selExercises.onfocusout=function(){
--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,22 +1,25 @@
 var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
-                  'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
+                  'Keylime Pie','Cherry Cobbler','Chocolate Chip Cookies']
 
+// populate the list using dessert array
 dessertVoting.onshow=function(){
+    // always clear before populating
     drpDesserts.clear()   
+    // put array of desserts in the dropdown
     for (i=0; i<=dessertList.length - 1; i++)
     drpDesserts.addItem(dessertList[i])
 }
 
-
-drpDesserts.onclick=function(dessertList){
-    if (typeof(dessertList) == "object")   
-      return                    
-    else {  // the user picked something
-       let textDessert = NSB.$("drpDesserts_" + dessertList).textContent
-       lbl.Message1.value = (`You picked ${textDessert} - that is a great choice!`)
-    }
+drpDesserts.onclick=function(s){
+  if (typeof(s) == "object"){  // means control clicked but no selection made yet
+    return                     // do nothing
+  } else {
+    drpDesserts.value = s   // make dropdown show choice user made
+    lblMessage1.value = `You picked ${s} - that is a great choice!`
+  }
 }
 
 btnYou.onclick=function () {
 ChangeForm(describeYou)
 }
+
